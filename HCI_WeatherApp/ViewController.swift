546ed3671b97d84f9dfce082bc61b9b564cdb667
Alina Godun <godun.lina@gmail.com>
2018-04-08 18:57:36 +0200

    resolving commit problem

---
+++ HCI_WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HCI_WeatherApp
+//
+//  Created by Alina Godun on 08.04.18.
+//  Copyright © 2018 Alina Godun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
